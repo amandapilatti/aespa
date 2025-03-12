2f6883235810c492b31836554fc1446c7da1f248
Nanda <143842040+amandapilatti@users.noreply.github.com>
2025-03-12 19:34:03 +0000

    editei script e styles

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    const alternaContraste = document.getElementById('alterna-contraste');
 
     let tamanhoAtualFonte = 1;
 
@@ -20,5 +21,9 @@ document.addEventListener('DOMContentLoaded', function () {
     diminuiFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    })
+
+    alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
     })
 })
